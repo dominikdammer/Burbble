d7f29c830b71a6b8c2f429df82a37cefeed5f5b4
Martin Kruse <martin.kruse.friedrichshain@gmail.com>
2025-01-25 12:40:16 +0100

    Fix Level (again)

--- Burbble/Assets/Scripts/ScriptLevel.cs
+++ Burbble/Assets/Scripts/ScriptLevel.cs
@@ -1,20 +1,12 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq.Expressions;
 using UnityEngine;
-using UnityEngine.UIElements;
-
-
 
 [CreateAssetMenu(fileName = "NewLevel", menuName = "Level/Create New Level")]
 public class Level : ScriptableObject
 {
-
     [Header("Fischarten (6 Werte)")]
-    public int[] fischarten = new int[6];
+    public int[] intFischarten = new int[6];
 
     [Header("Zieltöne (6 Werte)")]
-    public int[] zieltöne = new int[6];
+    public int[] intZieltöne = new int[6];
 }
 
