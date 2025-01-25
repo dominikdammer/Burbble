02acd0caf5e734c9eb98be417904283b137ee653
SterzMaxi <20960760+SterzMaxi@users.noreply.github.com>
2025-01-25 20:36:52 +0100

    glass Change color and drink gets transparent when poured

--- Burbble/Assets/InventoryFish/Script/SlotFish.cs
+++ Burbble/Assets/InventoryFish/Script/SlotFish.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SlotFish : MonoBehaviour, IDropHandler
 {
@@ -9,12 +10,20 @@ public class SlotFish : MonoBehaviour, IDropHandler
     public Mix mix;
     public Game game;
     public int IndexFish;
-    public int DrinkValue;
+    private Image DrinkImage;
+
+    [SerializeField] DrinkColorChanger drinkColorChanger;
     public void OnDrop(PointerEventData eventData)
     {
         GameObject dropped = eventData.pointerDrag;
         DrinkScript draggableItem = dropped.GetComponent<DrinkScript>();
+        drinkColorChanger.ChangeColor(draggableItem.DrinkValue);
 
+        DrinkImage = dropped.GetComponent<Image>();
+        var tempColor = DrinkImage.color;
+        tempColor.a = 0f;
+        DrinkImage.color = tempColor;
+        
         game.AssigneDrink(IndexFish);
 
         if (draggableItem == null)
@@ -28,8 +37,10 @@ public class SlotFish : MonoBehaviour, IDropHandler
             draggableItem.CanDrag = false;
         }
         else{
+            Debug.Log("destroy drink");
             Transform existingChild = transform.GetChild(0);
-            Destroy(existingChild.gameObject);
+            existingChild.gameObject.GetComponent<DrinkScript>().ResetDrink();
+            //Destroy(existingChild.gameObject);
             draggableItem.parentAfterDrag = transform;
             draggableItem.CanDrag = false;
         }
